94cceb327934d2971da259c1dc45d268dae63dcb
grafica <paolo@monema.it>
2021-11-12 18:51:01 +0100

    gestione mobile in css, problema con gatsby con l'oggetto window durante la build

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,29 +4,19 @@ import NavBar from "./navbar/navbar"
 import NavBarMobile from "./navbar/navbarMobile"
 import Breadcrumb from "./breadcrumb/breadCrumb"
 
-
+const isBrowser = typeof window !== "undefined"
 
 const Header = ({ locale, translations, pageTitle, pathName, currentPath, nodeType }) => {
 
   const [mega, setMega] = React.useState(false)
 
-  //gestione menu per il mobile
-  const [windowWidth, setWindowWidth] = React.useState()
-
-  React.useEffect(() => {
-    windowWidth ?
-    window.addEventListener('resize', () => setWindowWidth(window.innerWidth)) :
-    setWindowWidth(window.innerWidth)
-  })
-
-  const finestraMobile = (windowWidth <= 1024) && true
-  console.log(finestraMobile);
+ 
 
   return (
     <header>
-      {!finestraMobile ?
+  
         <NavBar locale={locale} translations={translations} currentPath={currentPath} pathName={pathName} mega={mega} setMega={setMega} />
-        : <NavBarMobile locale={locale} translations={translations} currentPath={currentPath} pathName={pathName} mega={mega} setMega={setMega} />}
+        <NavBarMobile locale={locale} translations={translations} currentPath={currentPath} pathName={pathName} mega={mega} setMega={setMega} />
 
       {pageTitle !== 'Home' ? <Breadcrumb pageTitle={pageTitle} pathName={currentPath} nodeType={nodeType} /> : ''}
 
