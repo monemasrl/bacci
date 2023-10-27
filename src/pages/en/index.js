c65d7b3bbc78ebc4267a1b37bcd0c9febd74dd7a
grafica <paolo@monema.it>
2023-10-27 17:52:33 +0200

    test build errore memory

--- src/pages/en/index.js
+++ src/pages/en/index.js
@@ -3,24 +3,13 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../../components/layout/layout"
 
-
 const IndexPage = () => (
-<>
-   
+  <>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-     
-    </p>
+
+    <p></p>
   </>
 )
 
