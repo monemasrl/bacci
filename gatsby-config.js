5cff9a3784b91ffb9cd194b245af8b203ae97d46
grafica <paolo@monema.it>
2023-10-30 11:25:10 +0100

    configurazione indirizzo gatsby wordpress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: `https://bacci-bedrock.monema.dev/wp/graphql`,
+        url: `https://bacci.monema.dev/graphql`,
       },
     },
     `gatsby-plugin-react-helmet`,
