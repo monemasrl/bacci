e78908d9acf9c7a3dcc1b85d25ae5c3317c00466
grafica <paolo@monema.it>
2023-10-30 22:24:19 +0100

    template home funzionante

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,75 +1,72 @@
 exports.createPages = async ({ graphql, actions: { createPage } }) => {
-
-  const result = await graphql(` 
-   { 
-    allWpFiera {
-      edges {
-      node {
-        content
-          fiere {
-          dataDa
-          dataA
-          luogo
-          link
-          sottotitolo
-          descrizione
-          personale {
-            ... on WpPersonale {
-              id
-              title
+  const result = await graphql(`
+    {
+      allWpFiera {
+        edges {
+          node {
+            content
+            fiere {
+              dataDa
+              dataA
+              luogo
+              link
+              sottotitolo
+              descrizione
               personale {
-                immagine{
-                  altText
-                  localFile {
-                  childImageSharp {
-                      gatsbyImageData(
-                      width: 238
-                      placeholder: BLURRED
-                      formats: [AUTO, WEBP, AVIF]
-
-                      )
+                ... on WpPersonale {
+                  id
+                  title
+                  personale {
+                    immagine {
+                      altText
+                      localFile {
+                        childImageSharp {
+                          gatsbyImageData(
+                            width: 238
+                            placeholder: BLURRED
+                            formats: [AUTO, WEBP, AVIF]
+                          )
+                        }
+                      }
+                    }
+                    link
+                    nome
+                    professione
+                    cognome
                   }
-                  }
+                }
               }
-                link
-                nome
-                professione
-                cognome
+              callToAction {
+                paragrafo
               }
             }
-          }
-          callToAction {
-            paragrafo
-          }
-  
-          }
-          title
-          slug
-          locale {
-        locale
-      }
-      translations {
-        href
-        id
-        locale
-        post_title
-      }
-      }
-      }
-    }   
-        wpPersonale {
-          id
-          title
-          personale {
-            immagine {
+            title
+            slug
+            locale {
+              locale
+            }
+            translations {
+              href
               id
+              locale
+              post_title
             }
-            link
-            nome
-            professione
-            cognome
           }
         }
+      }
+      wpPersonale {
+        id
+        title
+        personale {
+          immagine {
+            id
+          }
+          link
+          nome
+          professione
+          cognome
+        }
+      }
       allWpPost {
         edges {
           node {
@@ -101,39 +98,36 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
               }
             }
             prodotto {
-    
               inEvidenza
               paragrafo
               software
               sottotitolo
               testoAnteprima
-              immagine{
+              immagine {
                 altText
                 localFile {
-                childImageSharp {
-                    gatsbyImageData(
-                    width: 900
-                    placeholder: BLURRED
-                    formats: [AUTO, WEBP, AVIF]
-
-                    )
-                }
-                }
-            }
-              sezioniProdotto {
-                immagine{
-                  altText
-                  localFile {
                   childImageSharp {
-                      gatsbyImageData(
-                      width: 728
+                    gatsbyImageData(
+                      width: 900
                       placeholder: BLURRED
                       formats: [AUTO, WEBP, AVIF]
-
-                      )
+                    )
                   }
-                  }
+                }
               }
+              sezioniProdotto {
+                immagine {
+                  altText
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(
+                        width: 728
+                        placeholder: BLURRED
+                        formats: [AUTO, WEBP, AVIF]
+                      )
+                    }
+                  }
+                }
                 paragrafo
                 titolo
               }
@@ -153,57 +147,88 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
           }
         }
       }
-  }
-    `);
-
+    }
+  `)
+  const dataForLanguagePath = await graphql(`
+    {
+      allWpPage {
+        edges {
+          node {
+            title
+            slug
+            pathPagine {
+              fieldGroupName
+              path
+            }
+            locale {
+              locale
+            }
+          }
+        }
+      }
+    }
+  `)
   const fiere = result.data.allWpFiera.edges
   const langTag = {
-    en_US: 'en',
-    it_IT: 'it'
+    en_US: "en",
+    it_IT: "it",
   }
   const Termini = {
     en_US: {
-            prodotti: 'products'   
+      prodotti: "products",
     },
     it_IT: {
-            prodotti: 'prodotti' 
-              },
+      prodotti: "prodotti",
+    },
   }
 
+  dataForLanguagePath.data.allWpPage.edges.forEach(entry => {
+    if (entry.node.slug === "home") {
+      const path =
+        langTag[entry.node.locale.locale] === "it"
+          ? "/"
+          : langTag[entry.node.locale.locale] + "/"
 
+      createPage({
+        path: path,
+        component: require.resolve("./src/templates/home.jsx"),
+        context: {
+          lang: entry.node.locale.locale,
+        },
+      })
+    }
+  })
 
-  fiere.forEach((entry) => {
-
-    const urlBase = langTag[entry.node.locale.locale] === 'it' ? '/' : `/${langTag[entry.node.locale.locale]}/`
-
-
+  fiere.forEach(entry => {
+    const urlBase =
+      langTag[entry.node.locale.locale] === "it"
+        ? "/"
+        : `/${langTag[entry.node.locale.locale]}/`
 
     createPage({
       path: `${urlBase}fiere/${entry.node.slug}`,
-      component: require.resolve('./src/templates/fiere.jsx'),
+      component: require.resolve("./src/templates/fiere.jsx"),
       context: {
         content: entry.node,
         locale: entry.node.locale.locale,
         translations: entry.node.translations,
         slug: entry.node.slug,
         parentPath: entry.node.fiere.path,
-      }
+      },
     })
-
-
   })
 
   const news = result.data.allWpPost.edges
 
-  news.forEach((entry) => {
-
-    const urlBase = langTag[entry.node.locale.locale] === 'it' ? '/' : `/${langTag[entry.node.locale.locale]}/`
-
-
+  news.forEach(entry => {
+    const urlBase =
+      langTag[entry.node.locale.locale] === "it"
+        ? "/"
+        : `/${langTag[entry.node.locale.locale]}/`
 
     createPage({
       path: `${urlBase}news/${entry.node.slug}`,
-      component: require.resolve('./src/templates/news.jsx'),
+      component: require.resolve("./src/templates/news.jsx"),
       context: {
         content: entry.node.content,
         locale: entry.node.locale.locale,
@@ -211,30 +236,29 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
         translations: entry.node.translations,
         slug: entry.node.slug,
         title: entry.node.title,
-        date: entry.node.date
-       
-      }
+        date: entry.node.date,
+      },
     })
-
-
   })
 
-
-  result.data.allWpProdotto.edges.forEach((entry) => {
-    const urlBase = langTag[entry.node.locale.locale] === 'it' ? '/' : `/${langTag[entry.node.locale.locale]}/`
+  result.data.allWpProdotto.edges.forEach(entry => {
+    const urlBase =
+      langTag[entry.node.locale.locale] === "it"
+        ? "/"
+        : `/${langTag[entry.node.locale.locale]}/`
 
     createPage({
-      path: `${urlBase}${Termini[entry.node.locale.locale].prodotti}/${entry.node.slug}`,
-      component: require.resolve('./src/templates/prodotto.jsx'),
+      path: `${urlBase}${Termini[entry.node.locale.locale].prodotti}/${
+        entry.node.slug
+      }`,
+      component: require.resolve("./src/templates/prodotto.jsx"),
       context: {
         content: entry.node,
         locale: entry.node.locale.locale,
         translations: entry.node.translations,
         slug: entry.node.slug,
         title: entry.node.title,
-
       },
     })
   })
-
 }
