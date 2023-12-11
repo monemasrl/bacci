777038989061fcdc91838fdb8b022a360ff39723
grafica <paolo@monema.it>
2023-12-11 18:34:39 +0100

    test 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,11 @@
 import * as React from "react"
 
-
-
 const NotFoundPage = () => (
-<>
-
+  <div className="page404">
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </>
+    <p>Ci dispiace, la pagina che hai richiesto non esiste!</p>
+    <a href="/">Torna alla home</a>
+  </div>
 )
 
 export default NotFoundPage
