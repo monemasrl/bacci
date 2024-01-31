1416a1691639ef51fa7f2bc44060bf28f3c084c4
grafica <paolo@monema.it>
2024-01-31 15:59:28 +0100

    scroll to top al pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -24,7 +24,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, topArchivio }) => {
                                 onClick={() => {
                                     setActivePage(item)
                                     paginate(item)
-
+                                    window.scrollTo(0, 0)
                                 }}>{item}</li>
                         })
                     }
