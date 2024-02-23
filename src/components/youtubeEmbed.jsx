f879699869da567a1f7eef2ee9c55e43e4ac3b4c
grafica <paolo@monema.it>
2024-02-23 11:57:24 +0100

    modifica iframe youtube

--- src/components/youtubeEmbed.jsx
+++ src/components/youtubeEmbed.jsx
@@ -3,12 +3,8 @@ import React from "react";
 
 const YoutubeEmbed = ({ embedId }) => (
     <div className="video-responsive">
-        <iframe
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/embed/${embedId}?autoplay=0`}
-            title="Embedded youtube"
-        />
+        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${embedId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
     </div>
 );
 
