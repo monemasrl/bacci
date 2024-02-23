ba7dcbb66fbc845c43ef5318ebbd09c1d2d3877d
grafica <paolo@monema.it>
2024-02-23 11:47:52 +0100

    modifica iframe youtube

--- src/components/youtubeEmbed.jsx
+++ src/components/youtubeEmbed.jsx
@@ -6,9 +6,7 @@ const YoutubeEmbed = ({ embedId }) => (
         <iframe
             width="853"
             height="480"
-            src={`https://www.youtube.com/embed/${embedId}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
+            src={`https://www.youtube.com/embed/${embedId}?autoplay=0`}
             title="Embedded youtube"
         />
     </div>
