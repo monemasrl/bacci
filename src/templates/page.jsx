5ec922011cff17fd5cec27a43cc25d2396557a97
grafica <paolo@monema.it>
2025-07-22 15:46:32 +0200

    test build

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -36,7 +36,7 @@ export const query = graphql`
         id
         childImageSharp{
           id
-          gatsbyImageData(formats: [WEBP], quality: 70, placeholder: BLURRED, breakpoints: [ 440,  1024, 1920])
+          gatsbyImageData(formats: [WEBP], quality: 70, placeholder: BLURRED, breakpoints: [ 440, 1920])
         }
       }
     }
@@ -62,7 +62,7 @@ export const query = graphql`
               imageFile{
                 id
                 childImageSharp{
-                  gatsbyImageData(formats: [WEBP], quality: 70, placeholder: BLURRED, breakpoints: [ 440,  1024, 1920])
+                  gatsbyImageData(formats: [WEBP], quality: 70, placeholder: BLURRED, breakpoints: [ 440, 1920])
                 }
 
               }
