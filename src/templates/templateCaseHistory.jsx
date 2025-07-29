fc9249acdfc1346db0684c41286b7731a55efa68
grafica <paolo@monema.it>
2025-07-29 23:09:11 +0200

    controllo lingua su correlati case history

--- src/templates/templateCaseHistory.jsx
+++ src/templates/templateCaseHistory.jsx
@@ -108,7 +108,7 @@ const TemplateCaseHistory = ({ pageContext }) => {
                                 <ul>
                                     {content.related_machines.map((item, index) => {
                                         const translated = findItemTranslated(item.Prodotti_id.translations, locale)
-                                        if (translated) { return <li key={index}><Link to={`/${locale === 'it_IT' ? '' : langTag[locale] + '/'}${Termini[locale].prodotti}/${translated.slug}`}>{translated.titolo}</Link></li> } else { return null }
+                                        if (translated.titolo && translated.slug) { return <li key={index}><Link to={`/${locale === 'it_IT' ? '' : langTag[locale] + '/'}${Termini[locale].prodotti}/${translated.slug}`}>{translated.titolo}</Link></li> } else { return null }
                                     })}
                                 </ul>
                             </li>}
