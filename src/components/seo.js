22e2c6ea052d586816bc541c85a48ffcfaa4e0f8
grafica <paolo@monema.it>
2025-11-05 16:29:13 +0100

    integrati i hreflang

--- src/components/seo.js
+++ src/components/seo.js
@@ -42,22 +42,34 @@ function Seo({ description, lang, meta, title, seo, allPagePath }) {
           content: seo.title,
         })
       }
-      if (seo.description) {
+      if (seo.meta_description) {
         arrSeo.push({
           property: `og:description`,
-          content: seo.description,
+          content: seo.meta_description,
         })
       }
-      if (seo.type) {
+      if (langTag[lang]) {
         arrSeo.push({
-          property: `og:type`,
-          content: seo.type,
+          property: `og:locale`,
+          content: langTag[lang],
         })
       }
+      if (localePath?.path) {
+        arrSeo.push({
+          property: `og:url`,
+          content: site.siteMetadata.siteUrl + localePath?.path,
+        })
+      }
+
+      arrSeo.push({
+        property: `og:site_name`,
+        content: 'bacci.com',
+      })
+
     }
     return arrSeo
   }
-
+  console.log(seo)
   return (
     <Helmet >
       <html lang={langTag[lang]} />
@@ -67,7 +79,15 @@ function Seo({ description, lang, meta, title, seo, allPagePath }) {
         content={seo?.meta_description || metaDescription}
       />
       <meta name="author" content={site.siteMetadata.author} />
-      <link rel="canonical" href={localePath?.path} />
+      <link rel="canonical" href={site.siteMetadata.siteUrl + localePath?.path} />
+      {allPagePath && allPagePath.map((item, idx) => (
+        <link
+          key={item.locale}
+          rel="alternate"
+          hrefLang={item.locale === "it" ? "it" : "en"}
+          href={site.siteMetadata.siteUrl + item.path}
+        />
+      ))}
       {seo &&
         getDataSeoOpenGraph(seo).map((item, index) => {
           return <meta key={index} property={index} {...item} />
