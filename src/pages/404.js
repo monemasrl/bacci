0ea05a35cc67bdef65b2b1e88c64df63f24fc65d
grafica <paolo@monema.it>
2025-11-06 16:10:11 +0100

    404 netlify

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,23 @@
 import * as React from "react"
+import Layout from "../components/layout/layout"
 
-const NotFoundPage = () => (
-  <div className="page404">
-    <h1>404: Not Found</h1>
-    <p>Ci dispiace, la pagina che hai richiesto non esiste!</p>
-    <a href="/">Torna alla home</a>
-  </div>
-)
+const NotFoundPage = ({ pageContext }) => {
+  return (
+    <Layout
+      locale={pageContext?.locale || "it_IT"}
+      pageTitle="404 - Pagina non trovata"
+      tipo="page404"
+    >
+      <div className="container">
+        <div className="page404">
+          <h1>404: Not Found</h1>
+          <p>Ci dispiace, la pagina che hai richiesto non esiste!</p>
+          <a href="/">Torna alla home</a>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
+
